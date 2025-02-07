24c043b7942615f35ff80e666ba2d5a5b7a5a814
PedroPimpao <pedrohmpimpao@gmail.com>
2025-02-07 18:31:41 -0300

    Criacao da pasta "Testes"

---
+++ Testes/valor_aleatorio/app.js
@@ -0,0 +1,4 @@
+const array=[10,20,30,40,50,60,70,80,90,100]
+const randomIndex=Math.floor(Math.random()*array.length)
+const randomElement=array[randomIndex]
+console.log(randomElement)
---
+++ Testes/valor_aleatorio/script.js
@@ -0,0 +1,4 @@
+const valores=[1,2,3]
+const indiceAleatorio=Math.floor(Math.random()*valores.length)
+const valorAleatorio=valores[indiceAleatorio]
+console.log(valorAleatorio)
