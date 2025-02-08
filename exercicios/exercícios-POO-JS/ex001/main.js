7619db74530b5a2fc594bb606857bbe3fdae6046
PedroPimpao <pedrohmpimpao@gmail.com>
2025-02-08 20:43:18 -0300

    Update main.js

--- exercicios/exercícios-POO-JS/ex001/main.js
+++ exercicios/exercícios-POO-JS/ex001/main.js
@@ -1,2 +1,24 @@
 // Crie uma classe chamada “Círculo” que possua um atributo para armazenar o raio e métodos para calcular a área e o perímetro do círculo.
+const valor_pi=3.14
+class Circulo{
+    constructor(input_raio){
+        this.raio=input_raio
+    }
+    calc_area(){
+        this.area=valor_pi*(this.raio**2)
+        return this.area.toFixed(2)
+    }
+    calc_perimetro(){
+        this.perimetro=2*valor_pi*this.raio
+        return this.perimetro.toFixed(2)
+    }
+    info(){
+        console.log(`Area: ${this.calc_area()}`)
+        console.log(`Perímetro: ${this.calc_perimetro()}`)
+    }
+}
 
+const circulo= new Circulo(5)
+circulo.info()
+
+// Feito
