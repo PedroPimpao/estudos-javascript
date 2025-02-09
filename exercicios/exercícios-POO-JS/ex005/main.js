a5960b1dfe9da512a10df41a4f29c375c4ab894f
PedroPimpao <pedrohmpimpao@gmail.com>
2025-02-08 22:08:19 -0300

    Update main.js

--- exercicios/exercícios-POO-JS/ex005/main.js
+++ exercicios/exercícios-POO-JS/ex005/main.js
@@ -1,1 +1,10 @@
 // Crie uma classe chamada “Funcionário” com atributos para armazenar o nome, o salário e o cargo do funcionário. Implemente métodos para calcular o salário líquido, considerando descontos de impostos e benefícios.
+class Funcionario{
+    constructor(input_nome,input_salario,input_cargo){
+        this.nome=input_nome
+        this.salario=input_salario
+        this.cargo=input_cargo
+    }
+
+}
+// Em desenvolvimento...
