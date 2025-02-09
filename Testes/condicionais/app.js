34de2d0c70e0c4a0172909d3aac483de42eda74a
PedroPimpao <pedrohmpimpao@gmail.com>
2025-02-09 19:58:12 -0300

    Create app.js

---
+++ Testes/condicionais/app.js
@@ -0,0 +1,12 @@
+const day='Sexta-Feira'
+const dayObject={
+    'Domingo':1,
+    'Segunda-Feira':2,
+    'Terca-Feira':3,
+    'Quarta-Feira':4,
+    'Quinta-Feira':5,
+    'Sexta-Feira':6,
+    'Sabado':7
+}
+let dayOfObject=dayObject[day]
+console.log(dayOfObject)
