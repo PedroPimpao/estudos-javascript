ceff0569dfda288f917b8160bf2e815ff0657693
PedroPimpao <pedrohmpimpao@gmail.com>
2025-04-21 16:44:23 -0300

    Update main.js

--- exercicios/exercícios-POO-JS/ex015/main.js
+++ exercicios/exercícios-POO-JS/ex015/main.js
@@ -1,13 +1,55 @@
 // Crie uma classe chamada “MáquinaDeVendas” que simule uma máquina de venda de produtos. Essa classe deve permitir cadastrar produtos, selecionar um produto para compra, inserir dinheiro, retornar o troco e exibir o estoque disponível.
 
 class Produto{
-    constructor(){
-        
+    constructor(input_nome, input_preco, input_quantidade){
+        this.nome=input_nome
+        this.preco=input_preco
+        this.quantidade=input_quantidade
     }
 }
 
 class MaquinaDeVendas{
     constructor(){
+        this.lista_produtos=[]
+    }
 
+    mensagemListaVazia(){
+        console.log(`Lista de Produtos vazia!`)
+    }
+    cadastrarProduto(nome, preco, quantidade){
+        const novoProduto = new Produto(nome, preco, quantidade)
+        this.lista_produtos.push(novoProduto)
+        console.log("--- Novo Produto Cadastrado ---")
+        this.exibirProduto(novoProduto)
+        return novoProduto
+    }
+    removerProduto(produtoRemovido){
+        if(this.lista_produtos.length > 0){
+            this.lista_produtos=this.lista_produtos.filter((produto)=>{
+                return produto!==produtoRemovido
+            })
+            console.log("--- Produto Removido: ---")
+            this.exibirProduto(produtoRemovido)
+        }else{
+            this.mensagemListaVazia()
+        }
+    }
+    exibirProduto(produto){
+        const {nome, preco, quantidade}=produto
+        console.log("==============")
+        console.log(`Nome: ${nome}`)
+        console.log(`Preço: ${preco}`)
+        console.log(`Quantidade disponível: ${quantidade}`)
+        console.log("==============")
+    }
+    exibirLista(){
+        if(this.lista_produtos.length > 0){
+            this.lista_produtos.map((produto, indice)=>{
+                console.log(`- - - Produto ${indice+1} - - -`)
+                this.exibirProduto(produto)
+            })
+        }else{
+            this.mensagemListaVazia()
+        }
     }
 }
