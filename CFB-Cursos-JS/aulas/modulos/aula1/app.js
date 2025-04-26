1c7254ebea6c945f43457bf8236881d7d50d2eba
PedroPimpao <pedrohmpimpao@gmail.com>
2025-04-26 16:39:07 -0300

    aula Modulos

---
+++ CFB-Cursos-JS/aulas/modulos/aula1/app.js
@@ -0,0 +1,8 @@
+import pegarTodosCursos, { carros, cursos } from "./cursos.js"
+
+console.log('Rodando')
+console.log(carros)
+console.log(cursos)
+
+pegarTodosCursos()
+// console.log(pegarTodosCursos())
---
+++ CFB-Cursos-JS/aulas/modulos/aula1/cursos.js
@@ -0,0 +1,12 @@
+const cursos=['JavaScript', 'HTML', 'CSS', 'Python', 'C++']
+const carros=['Mustang', 'GT-R', '350Z', 'Camaro', 'Ferrari SF90']
+
+const pegarTodosCursos=()=>{
+    console.log(cursos)
+    return cursos
+}
+
+export {cursos, carros}
+export default pegarTodosCursos
+
+
