1b3b976a15f6979fb047961999da3a8d3ba75b04
PedroPimpao <pedrohmpimpao@gmail.com>
2025-04-26 15:26:41 -0300

    aula Objetos Literais P2

--- CFB-Cursos-JS/aulas/objetosLiterais/aula1/app.js
+++ CFB-Cursos-JS/aulas/objetosLiterais/aula1/app.js
@@ -6,11 +6,28 @@ const computador={
     hd: "2tb",
 
     info:function(){
+        console.log("Info: ")
         console.log(`CPU: ${this.cpu}`);
         console.log(`RAM: ${this.ram}`);
         console.log(`HD: ${this.hd}`);  
     }
 }
+
+delete(computador.hd)
+
+const c2=Object.create(computador)
+c2.info()
+
+const c1 = Object.assign({}, computador)
+c1.info()
+
+const o1={obj1: '1'}
+const o2={obj2: '2'}
+const o3={obj3: '3'}
+const o4=Object.assign(o1, o2, o3)
+console.log(o4);
+
+
 const computadores=[
     {
         cpu: "i9",
